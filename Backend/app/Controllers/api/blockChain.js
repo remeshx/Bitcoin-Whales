3fe55385c93c363ea7a8b5eaf032892b69ea616c
reza <meshkat@ymail.com>
2021-05-12 17:11:57 -0700

    remove logs

--- Backend/app/Controllers/api/blockChain.js
+++ Backend/app/Controllers/api/blockChain.js
@@ -379,7 +379,7 @@ class Blockchain {
                     for await (const vin of tx.vin) {
                         vinQueryCount++;
                         vtxidx = vin.txid.substring(0,3);
-                        console.log(`vtxidx ${txidx}`); 
+                        //console.log(`vtxidx ${txidx}`); 
                         if (txcounter>trxRead) 
                             vinQuery = vinQuery + `,(${readHeight},'${tx.txid}','${vtxidx}','${vin.txid}',${vin.vout})`;
                     };
@@ -399,7 +399,7 @@ class Blockchain {
 
                   // BlockChainModel.saveOutputs(transactionId,address,voutCounter,vout.value);
                   txidx = tx.txid.substring(0,3);
-                  console.log(`txidx ${txidx}`); 
+                  //console.log(`txidx ${txidx}`); 
                   if (txcounter>trxRead) 
                     voutQuery = voutQuery + `,(${readHeight},'${txidx}','${tx.txid}','${address}',${voutCounter},${vout.value})`;
                   voutQueryCount++;
