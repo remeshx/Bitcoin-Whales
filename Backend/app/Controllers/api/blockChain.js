bdb1cadf7ace82a93366c7a3a116d6b60bc87b0b
reza <meshkat@ymail.com>
2021-05-19 14:07:09 -0700

    writing to db from file - add index

--- Backend/app/Controllers/api/blockChain.js
+++ Backend/app/Controllers/api/blockChain.js
@@ -326,12 +326,12 @@ class Blockchain {
             
             if (tblName=='inputs') {         
              tblName = 'inputs_' + file.substring(8,11);
-             //await BlockChainModel.importInputFile(filepath,tblName); 
+             await BlockChainModel.importInputFile(filepath,tblName); 
              await BlockChainModel.createIndex('idx_'+tblName+'_vouttxid',tblName,'vouttxid'); 
              await BlockChainModel.createIndex('idx_'+tblName+'_vout',tblName,'vout'); 
             } else if (tblName=='output') {              
              tblName = 'outputs_' + file.substring(9,12);
-             //await BlockChainModel.importOutputFile(filepath,tblName); 
+             await BlockChainModel.importOutputFile(filepath,tblName); 
              await BlockChainModel.createIndex('idx_'+tblName+'_txid',tblName,'txid'); 
              await BlockChainModel.createIndex('idx_'+tblName+'_vout',tblName,'vout'); 
             }
