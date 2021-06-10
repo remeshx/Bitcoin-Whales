6c5dc112adc776f230abf7f0372fed39f39a2b56
reza <meshkat@ymail.com>
2021-06-09 18:11:58 -0700

    phase 4 updates

--- Backend/app/Controllers/api/blockChain.js
+++ Backend/app/Controllers/api/blockChain.js
@@ -382,6 +382,7 @@ class Blockchain {
     static async GenerateBitcoinAddressFiles(socket){
         //Phase4 : generate bitcoin address csv file
         console.log('Phase 4 - GenerateBitcoinAddressFiles ');           
+        this.fileStream = [];
         let chs = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
         let key='';
         let sql='';
