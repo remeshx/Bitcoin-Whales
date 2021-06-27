33e77d0b2797f32217dc9c9bb384d9ed1b8d8132
remeshx <45960079+remeshx@users.noreply.github.com>
2021-06-27 15:30:36 -0700

    Update whales.js

--- Backend/app/Controllers/whales.js
+++ Backend/app/Controllers/whales.js
@@ -50,6 +50,9 @@ class Whales {
                 for await (const vin of tx.vin) {                        
                     vtxidx =  vin.txid.substring(0,3);
                     txid = await BlockChainModel.getTransactionId(vtxidx,vin.txid);
+                    if (!txid) {
+                        throw 'TXid not found for ' + vin.txid;
+                    }
                     address = await Blockchain.getVInAddress(vin.txid,vin.vout);
                     vAddidx_ = address.trim().slice(-2); 
                     vAddidx = vAddidx_.charCodeAt(0) +''+ vAddidx_.charCodeAt(1); 
