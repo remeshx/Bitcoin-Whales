d47a43815e2f55c856817c01e9bd2172c2789506
remeshx <45960079+remeshx@users.noreply.github.com>
2021-11-04 00:01:31 -0700

    Update progress.js

--- Frontend/src/actions/progress.js
+++ Frontend/src/actions/progress.js
@@ -1,52 +1,48 @@
 import { Blocks } from './types';
 import { URL_PROGRESS_STATUS } from '../../config';
-global.x = 0;
+global.progressForTheFirstTime = true;
+global.progressRunning = false;
 
 export const updateStartupProgress = (data) => dispatch => {
-    console.log('global.x', global.x);
-    global.x += 1;
     console.log('updateStartupProgress', data);
-    if (! typeof progressForTheFirstTime !== 'undefined') var progressForTheFirstTime = true;
-    if (! typeof progressRunning !== 'undefined') var progressRunning = false;
-    console.log('progressForTheFirstTime', progressForTheFirstTime);
-    console.log('progressRunning', progressRunning);
-    if (progressRunning) return false;
+    console.log('progressForTheFirstTime', global.progressForTheFirstTime);
+    console.log('progressRunning', global.progressRunning);
+    if (global.progressForTheFirstTime || global.progressRunning) return false;
 
-    if (!progressRunning && progressForTheFirstTime) {
-        progressRunning = true;
-        let stepjson = {}
-        stepjson.step = 2;
-        console.log('data.step', data.step);
-        console.log('json.step', 2);
-        dispatch({ type: 'FETCH_PROGRESS_STATUS', progress: stepjson });
-        for (let i = 2; i <= parseInt(data.step); i++) {
-            setTimeout(() => {
-                let j = (i - 2) * 200
-                let json = {};
-                json.step = i;
-                console.log('data.step', data.step);
-                console.log('json.step', json.step);
-                console.log('timeout ' + i + '/' + j, json.step);
-                dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: json });
-                if (i == parseInt(data.step)) {
-                    console.log('STOPPED');
-                    progressForTheFirstTime = false;
-                    progressRunning = false;
-                }
-            }, (i - 2) * 200);
-        }
-    } else {
-        dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: data });
-    }
+    dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: data });
 }
 
 export const fetchProgressStatus = () => dispatch => {
+    console.log('updateStartupProgress', data);
+    console.log('progressForTheFirstTime', global.progressForTheFirstTime);
+    console.log('progressRunning', global.progressRunning);
 
     console.log('fetchProgressStatus');
     fetch(URL_PROGRESS_STATUS)
         .then(response => response.json())
         .then(data => {
-            updateStartupProgress(data);
+            global.progressRunning = true;
+            let stepjson = {}
+            stepjson.step = 2;
+            console.log('data.step', data.step);
+            console.log('json.step', 2);
+            dispatch({ type: 'FETCH_PROGRESS_STATUS', progress: stepjson });
+            for (let i = 2; i <= parseInt(data.step); i++) {
+                setTimeout(() => {
+                    let j = (i - 2) * 200
+                    let json = {};
+                    json.step = i;
+                    console.log('data.step', data.step);
+                    console.log('json.step', json.step);
+                    console.log('timeout ' + i + '/' + j, json.step);
+                    dispatch({ type: 'FETCH_PROGRESS_STATUS', progress: json });
+                    if (i == parseInt(data.step)) {
+                        console.log('STOPPED');
+                        global.progressForTheFirstTime = false;
+                        global.progressRunning = false;
+                    }
+                }, (i - 2) * 200);
+            }
         })
         .catch(error => console.log('error', error));
 }
