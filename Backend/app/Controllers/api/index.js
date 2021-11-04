e0b50337be491aba9bd45b183bbf8e41bdc1caa0
remeshx <45960079+remeshx@users.noreply.github.com>
2021-11-03 23:23:51 -0700

    updating api status

--- Backend/app/Controllers/api/index.js
+++ Backend/app/Controllers/api/index.js
@@ -2,85 +2,85 @@ const settingModel = require('../../Models/settings');
 const BlockChainModel = require('../../Models/blockchain');
 
 class Api {
-    static async getRichListStatus() {
-        let richlistTbl = await BlockChainModel.getRichListTable(100);
-        let getLastBlock = await  BlockChainModel.getLastBlockDetail();
+        static async getRichListStatus() {
+                let richlistTbl = await BlockChainModel.getRichListTable(100);
+                let getLastBlock = await BlockChainModel.getLastBlockDetail();
 
-        return new Promise((resolve,reject) => {
-               
-               
-                console.log('getLastBlock: ', getLastBlock);
-                let richlist = {
-                        'richlisttbl' : richlistTbl,
-                        'blockInfo' : getLastBlock
-                    }   
+                return new Promise((resolve, reject) => {
 
-                console.log('getRichListStatus Result: ', richlist);
-                resolve(richlist); 
-        });
-    }
 
-    static getLoadingStatus(){
+                        console.log('getLastBlock: ', getLastBlock);
+                        let richlist = {
+                                'richlisttbl': richlistTbl,
+                                'blockInfo': getLastBlock
+                        }
 
-        return new Promise((resolve,reject) => {
-                let action = global.settings['BitcoinNode_CurrentStage'];
-                // let action = 2;
-                // global.settings['BitcoinNode_LastFileWritten']=2090;
+                        console.log('getRichListStatus Result: ', richlist);
+                        resolve(richlist);
+                });
+        }
 
-                console.log('getLoadingStatus : ', action);
-                let step=0;
-                let currPos=0;
-                let finalPos=0;
+        static getLoadingStatus() {
 
-                switch (action) {
-                    case '1': 
-                            step=1;
-                            currPos = global.settings['BitcoinNode_currBlockHeightRead'];
-                            finalPos = global.settings['BitcoinNode_blockCount'];
-                            break;
-                    case '2': 
-                            step=2;
-                            currPos = global.settings['BitcoinNode_LastFileWritten'];
-                            finalPos = 4096;
-                            break;
-                    case '3': 
-                            step=3;
-                            currPos = global.settings['BitcoinNode_LastFileWritten'];
-                            finalPos = 4096;
-                            break;
-                    case '4': 
-                            step=4;
-                            currPos = global.settings['BitcoinNode_LastFileWritten'];
-                            finalPos = 3844;
-                            break;
-                    case '5': 
-                            step=5;
-                            currPos = 0;
-                            finalPos = 3844;
-                            break;      
-                    case '6': 
-                            step=6;
-                            currPos = 0;
-                            finalPos = 3844;
-                            break;  
-                    case '7': 
-                    case '10': 
-                            step=10;
-                            currPos = 0;
-                            finalPos = 0;
-                            break;   
-                }
+                return new Promise((resolve, reject) => {
+                        let action = global.settings['BitcoinNode_CurrentStage'];
+                        // let action = 2;
+                        // global.settings['BitcoinNode_LastFileWritten']=2090;
 
-                let progressStatus = {
-                    'step' : step,
-                    'currPos' : currPos,
-                    'finalPos' : finalPos
-                }
-                console.log('getLoadingStatus Result: ', progressStatus);
-                resolve(progressStatus); 
-        });
-    }
+                        console.log('getLoadingStatus : ', action);
+                        let step = 0;
+                        let currPos = 0;
+                        let finalPos = 0;
+
+                        switch (action) {
+                                case '1':
+                                        step = 1;
+                                        currPos = global.settings['BitcoinNode_currBlockHeightRead'];
+                                        finalPos = global.settings['BitcoinNode_blockCount'];
+                                        break;
+                                case '2':
+                                        step = 2;
+                                        currPos = global.settings['BitcoinNode_LastFileWritten'];
+                                        finalPos = 4096;
+                                        break;
+                                case '3':
+                                        step = 3;
+                                        currPos = global.settings['BitcoinNode_LastFileWritten'];
+                                        finalPos = 4096;
+                                        break;
+                                case '4':
+                                        step = 4;
+                                        currPos = global.settings['BitcoinNode_LastFileWritten'];
+                                        finalPos = 3844;
+                                        break;
+                                case '5':
+                                        step = 5;
+                                        currPos = 0;
+                                        finalPos = 3844;
+                                        break;
+                                case '6':
+                                        step = 6;
+                                        currPos = 0;
+                                        finalPos = 3844;
+                                        break;
+                                case '7':
+                                case '10':
+                                        step = 7;
+                                        currPos = 0;
+                                        finalPos = 0;
+                                        break;
+                        }
+
+                        let progressStatus = {
+                                'step': step,
+                                'currPos': currPos,
+                                'finalPos': finalPos
+                        }
+                        console.log('getLoadingStatus Result: ', progressStatus);
+                        resolve(progressStatus);
+                });
+        }
 
 }
 
-module.exports  = Api;
+module.exports = Api;
--- Frontend/src/actions/progress.js
+++ Frontend/src/actions/progress.js
@@ -45,29 +45,5 @@ export const updateStartupProgress = (data) => dispatch => {
     if (! typeof progressForTheFirstTime !== 'undefined') var progressForTheFirstTime = true;
     if (progressForTheFirstTime) return;
     dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: data });
-
-    // if (progressForTheFirstTime && data.step > 2) {
-    //     // progressRunning = true;
-    //     // let i = 2;
-    //     // let json = {};
-    //     // json.step = i;
-    //     // console.log('dispatch', json);
-    //     // dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: json });
-    //     // while (i <= parseInt(data.step)) {
-    //     //     i += 1;
-    //     //     json.step = i;
-    //     //     setTimeout(() => {
-    //     //         console.log('dispatch', json);
-    //     //         dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: json });
-    //     //         if (i == parseInt(data.step)) {
-    //     //             console.log('STOPPED');
-    //     //             progressForTheFirstTime = false;
-    //     //         }
-    //     //     }, (i - 2) * 1000);
-
-    //     // }
-    // } else if (!progressForTheFirstTime) {
-    //     //dispatch({ type: 'UPDATE_STARTUP_PROGRESS', progress: data });
-    // }
 }
 
